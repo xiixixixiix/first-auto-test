bc40b6fa5e4874b84db295147f14a0fdf26b3a1e
LiuYan <16684867746>
2024-02-17 15:57:10 +0800

    添加前端端口设置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port:8089,//更改端口号
+    open:true,//自动启动浏览器
+  }
 })
